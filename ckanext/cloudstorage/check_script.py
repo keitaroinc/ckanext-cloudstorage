5e2dc14b9d57a9f17cd2334f2ec0cef311efbba5
miloshIra <milosh.aritonski@keitaro.com>
2023-05-23 11:48:38 +0200

    added scripts to group and org templates

--- ckanext/cloudstorage/check_script.py
+++ ckanext/cloudstorage/check_script.py
@@ -4,7 +4,10 @@ from google.cloud import storage
 project_id = 'my-test-project'
 bucket_name = "datapusher-plus"
 
-
+""" 
+Runiing this script prints out the content of your google storage, created a dataset with an uploaded resource
+for example a .csv file, then run this script in the terminal, if your installation is successful your .csv file will be listed.
+"""
 
 storage_client = storage.Client.from_service_account_json(
         '/home/miloshira/Downloads/amplus-data-453d6f3dce42.json')
