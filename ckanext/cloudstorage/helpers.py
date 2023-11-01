fa5d3ba78128dcccf59682ed6c3b90c52f85b811
blagoja <blagojabozinovski@gmail.com>
2023-11-01 09:59:22 +0100

    Code cleanup

--- ckanext/cloudstorage/helpers.py
+++ ckanext/cloudstorage/helpers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import ckan.plugins.toolkit as tk
-
-
 from ckanext.cloudstorage.storage import ResourceCloudStorage
+import ckan.plugins.toolkit as tk
+from google.cloud import storage
+import datetime
+
+config = tk.config
 
 
 def use_secure_urls():
@@ -24,3 +26,31 @@ def use_multipart_upload():
 def max_upload_size():
     return tk.config.get("ckanext.cloudstorage.max_upload_size_gb")
 
+
+def generate_download_signed_url_v4(blob_name):
+    """Generates a v4 signed URL for downloading a blob.
+
+    Note that this method requires a service account key file. You can not use
+    this if you are using Application Default Credentials from Google Compute
+    Engine or from the Google Cloud SDK.
+    """
+
+    path_to_json = config.get(
+        'ckanext.cloudstorage.google_service_account_json')
+    bucket_name = config.get('ckanext.cloudstorage.container_name')
+    storage_client = storage.Client.from_service_account_json(path_to_json)
+    bucket = storage_client.bucket(bucket_name)
+
+    # storage_client = storage.Client()
+    # bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 24 hours
+        expiration=datetime.timedelta(hours=24),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+
+    return url
--- ckanext/cloudstorage/views.py
+++ ckanext/cloudstorage/views.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint
+from ckanext.cloudstorage.helpers import generate_download_signed_url_v4
 import ckantoolkit as tk
 import os
 import ckanext.cloudstorage.utils as utils
-from google.cloud import storage
-import datetime
 
 redirect = tk.redirect_to
 config = tk.config
@@ -25,49 +24,13 @@ def get_storage_path(upload_to):
     return os.path.join(path, 'storage', 'uploads', upload_to)
 
 
-def generate_download_signed_url_v4(blob_name):
-    """Generates a v4 signed URL for downloading a blob.
-
-    Note that this method requires a service account key file. You can not use
-    this if you are using Application Default Credentials from Google Compute
-    Engine or from the Google Cloud SDK.
-    """
-    # bucket_name = 'your-bucket-name'
-    # blob_name = 'your-object-name'
-    path_to_json = config.get(
-        'ckanext.cloudstorage.google_service_account_json')
-    bucket_name = config.get('ckanext.cloudstorage.container_name')
-    storage_client = storage.Client.from_service_account_json(path_to_json)
-    bucket = storage_client.bucket(bucket_name)
-
-    # storage_client = storage.Client()
-    # bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-
-    url = blob.generate_signed_url(
-        version="v4",
-        # This URL is valid for 15 minutes
-        expiration=datetime.timedelta(minutes=15),
-        # Allow GET requests using this URL.
-        method="GET",
-    )
-
-    print("Generated GET signed URL:")
-    print(url)
-    print("You can use this URL with any user agent, for example:")
-    print(f"curl '{url}'")
-    return url
-
-
 def uploaded_file_redirect(upload_to, filename):
     '''Redirect static file requests to their location on S3.'''
+    
     storage_path = get_storage_path(upload_to)
     filepath = os.path.join(storage_path, filename)
-    bucket = config.get('ckanext.cloudstorage.container_name')
-
     url = generate_download_signed_url_v4(filepath)
 
-    # url = 'https://previews.123rf.com/images/kchung/kchung1610/kchung161001354/64508202-test-written-by-hand-hand-writing-on-transparent-board-photo.jpg' 
     return redirect(url)
 
 
