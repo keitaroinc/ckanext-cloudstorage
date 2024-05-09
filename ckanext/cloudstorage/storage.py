84cfb7b50c1e20a095614a45d8bf48af993c728c
blagoja <blagojabozinovski@gmail.com>
2024-05-09 08:16:10 +0200

    Upload chunk size in config

--- ckanext/cloudstorage/storage.py
+++ ckanext/cloudstorage/storage.py
@@ -36,8 +36,9 @@ config = p.toolkit.config
 
 log = logging.getLogger(__name__)
 
-storage.blob._DEFAULT_CHUNKSIZE = 2 * 1024 * 1024  # 2 MB
-storage.blob._MAX_MULTIPART_SIZE = 2 * 1024 * 1024  # 2 MB
+chunk_size = int(config.get('ckanext.cloudstorage.chunk_size', '2'))
+storage.blob._DEFAULT_CHUNKSIZE = chunk_size * 1024 * 1024  # 2 MB default
+storage.blob._MAX_MULTIPART_SIZE = chunk_size * 1024 * 1024  # 2 MB default
 
 ALLOWED_UPLOAD_TYPES = (cgi.FieldStorage, FlaskFileStorage)
 AWS_UPLOAD_PART_SIZE = 5 * 1024 * 1024
